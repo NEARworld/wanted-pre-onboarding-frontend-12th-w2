74587f7286db0cceec918d7dd690b9bf0cc0eea3
nearworld <artemismars2@outlook.com>
2023-08-30 15:50:32 +0900

    feat: react-router-dom을 이용한 기본 라우터 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+
+import { RouterProvider } from 'react-router-dom';
+
+import { router } from './router/router';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
