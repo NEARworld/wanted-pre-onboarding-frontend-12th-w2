d086ed499d735e87a3dd92f5883439150f3a10a5
nearworld <artemismars2@outlook.com>
2023-08-30 17:33:05 +0900

    feat: 광고판 역할을 하는 Advertisement 컴포넌트 마크업

---
+++ src/components/Advertisement.tsx
@@ -0,0 +1,15 @@
+import { memo } from 'react';
+
+const Advertisement = () => {
+  return (
+    <a href='https://wanted.co.kr'>
+      <img
+        width='500px'
+        height='100px'
+        src='https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Fuserweb%2Flogo_wanted_black.png&w=110&q=100'
+      />
+    </a>
+  );
+};
+
+export const MemoizedAdvertisement = memo(Advertisement);
