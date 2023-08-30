0ed47fb8da9fddfca4cd8f3a3c22935d122d04d3
nearworld <artemismars2@outlook.com>
2023-08-30 18:13:46 +0900

    feat: Issue에서 IssueDetail로 패스된 데이터를 IssueDetail 에서 사용할 수 있도록 구현

--- src/components/IssueDetail.tsx
+++ src/components/IssueDetail.tsx
@@ -1,7 +1,22 @@
+import { useLocation } from 'react-router-dom';
+
+import { MemoizedIssue, Props as IssueProps } from './Issue';
+
 export const IssueDetail = () => {
-  return (
-    <div>
-      {/*<MemoizedIssue id={id} title={title} author={author} date={date} comments={comments} />*/}
-    </div>
-  );
+  const navigateState = useLocation().state ?? 100;
+
+  const validateState = (state: IssueProps): state is IssueProps => {
+    if (!state) return false;
+    return typeof state === 'object';
+  };
+
+  const { id, title, author, date, comments } = navigateState as IssueProps;
+
+  if (validateState(navigateState))
+    return (
+      <div>
+        <MemoizedIssue id={id} title={title} author={author} date={date} comments={comments} />
+      </div>
+    );
+  return null;
 };
