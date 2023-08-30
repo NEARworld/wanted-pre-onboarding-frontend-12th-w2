32f9e86d360d609e70461c2519430800ea74d932
nearworld <artemismars2@outlook.com>
2023-08-30 15:53:23 +0900

    feat: global styles에 reset css 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 
 import { RouterProvider } from 'react-router-dom';
 
-import { router } from './router/router';
+import { router } from 'router/router';
+import { GlobalStyle } from 'styles/globalStyles';
 import './App.css';
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
