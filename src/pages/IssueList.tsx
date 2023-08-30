fd7bcd4b345dfa41c76b69a6ac92226770b43cc3
nearworld <artemismars2@outlook.com>
2023-08-30 19:48:50 +0900

    refactor: filterIssue 함수 추가로 삼항 연산자 제거

--- src/pages/IssueList.tsx
+++ src/pages/IssueList.tsx
@@ -10,6 +10,7 @@ export type IssueType = {
   title: string;
   comments: number;
   created_at: string;
+  body: string;
   user: {
     login: string;
     avatar_url: string;
@@ -18,30 +19,22 @@ export type IssueType = {
 
 const OWNER = 'facebook';
 const REPO = 'react';
+const AD_LOCATION = 5;
 
 export const IssueList = () => {
   const { issues, isLoading } = useIssueList({ owner: OWNER, repo: REPO });
-  const checkIdxForAd = (idx: number) => (idx + 1) % 5;
+
+  const filterIssue = (idx: number, issue: IssueType) => {
+    const isAdLocation = !((idx + 1) % AD_LOCATION);
+    if (isAdLocation) return <MemoizedIssue key={idx} {...issue} />;
+    return <MemoizedAdvertisement key={idx} />;
+  };
 
   if (!isLoading)
     return (
       <StyledContainer>
         <IssueHeader owner={OWNER} repo={REPO} />
-        {issues &&
-          issues.map(({ number, title, user, created_at, comments }, idx) =>
-            checkIdxForAd(idx) ? (
-              <MemoizedIssue
-                key={idx}
-                number={number}
-                title={title}
-                user={user}
-                created_at={created_at}
-                comments={comments}
-              />
-            ) : (
-              <MemoizedAdvertisement key={idx} />
-            ),
-          )}
+        {issues && issues.map((issue, idx) => filterIssue(idx, issue))}
       </StyledContainer>
     );
   return <span>loading...</span>;
