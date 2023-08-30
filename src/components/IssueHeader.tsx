64158463352fde1486858c60f0f7145d469d7b4c
nearworld <artemismars2@outlook.com>
2023-08-30 17:32:09 +0900

    feat: IssueHeader 컴포넌트 마크업

---
+++ src/components/IssueHeader.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const IssueHeader = () => {
+  return <StyledTitle>header</StyledTitle>;
+};
+
+const StyledTitle = styled.h1`
+  text-align: center;
+  font-size: 30px;
+`;
