264dae53d48baac3a2f16e391d1cbef37b71ab94
nearworld <artemismars2@outlook.com>
2023-08-30 19:14:59 +0900

    feat: issue list 불러오는 동안 loading 텍스트 추가

--- src/pages/IssueList.tsx
+++ src/pages/IssueList.tsx
@@ -23,12 +23,15 @@ const REPO = 'react';
 
 export const IssueList = () => {
   const [issues, setIssues] = useState<IssueType[]>();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     getIssueList({ owner: OWNER, repo: REPO, page: 1 })
       .then(res => {
         const { status, data } = res;
         if (status) setIssues(data);
+        setIsLoading(false);
       })
       .catch(e => console.log(e));
   }, []);
@@ -39,26 +42,28 @@ export const IssueList = () => {
 
   const checkIdxForAd = (idx: number) => (idx + 1) % 5;
 
-  return (
-    <StyledContainer>
-      <IssueHeader owner={OWNER} repo={REPO} />
-      {issues &&
-        issues.map(({ number, title, user, created_at, comments }, idx) =>
-          checkIdxForAd(idx) ? (
-            <MemoizedIssue
-              key={idx}
-              number={number}
-              title={title}
-              user={user}
-              created_at={created_at}
-              comments={comments}
-            />
-          ) : (
-            <MemoizedAdvertisement key={idx} />
-          ),
-        )}
-    </StyledContainer>
-  );
+  if (!isLoading)
+    return (
+      <StyledContainer>
+        <IssueHeader owner={OWNER} repo={REPO} />
+        {issues &&
+          issues.map(({ number, title, user, created_at, comments }, idx) =>
+            checkIdxForAd(idx) ? (
+              <MemoizedIssue
+                key={idx}
+                number={number}
+                title={title}
+                user={user}
+                created_at={created_at}
+                comments={comments}
+              />
+            ) : (
+              <MemoizedAdvertisement key={idx} />
+            ),
+          )}
+      </StyledContainer>
+    );
+  return <span>loading...</span>;
 };
 
 const StyledContainer = styled.div`
