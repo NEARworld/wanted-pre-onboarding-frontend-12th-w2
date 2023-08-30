a8c667902df0bbf2e8eb2f719523e6d6d40446e7
nearworld <artemismars2@outlook.com>
2023-08-30 18:51:07 +0900

    feat: github api를 이용한 getIssueList 함수 구현

---
+++ src/apis/config.ts
@@ -0,0 +1,19 @@
+import { Octokit } from 'octokit';
+
+const octokit = new Octokit({
+  auth: process.env.REACT_APP_ACCESS_TOKEN!,
+});
+
+export const getIssueList = async (data: { owner: string; repo: string; page: number }) => {
+  const { owner, repo, page } = data;
+  return await octokit.request(
+    `GET /repos/${owner}/${repo}/issues?state=open&sort=comments&page=${page}`,
+    {
+      owner,
+      repo,
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28',
+      },
+    },
+  );
+};
